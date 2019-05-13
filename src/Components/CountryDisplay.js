4d318fb163ba2d13b7b1d68f5f57b47e19032a12
Bru <annabru.abh@gmail.com>
2019-05-13 11:57:19 +0200

    Created four main Components needed for now - each of them is a class component.

---
+++ src/Components/CountryDisplay.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CountryDisplay extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CountryDisplay
---
+++ src/Components/CountryItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CountryItem extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CountryItem
