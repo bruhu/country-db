a01bd2907433551f5da04527238b03ada1176b4f
Bru <annabru.abh@gmail.com>
2019-05-14 11:26:34 +0200

    Wrote a fetch in constructor function of CountryItem.js

--- src/Components/CountryItem.js
+++ src/Components/CountryItem.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
+import { classExpression } from "@babel/types";
 
 export class CountryItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+      // in some cases may be better to put [] instead of null
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then(resp => resp.json())
+      .then(data => this.setState({ data }));
+  }
+
   render() {
     return (
       <React.Fragment>
