8fc10cd35627b0d00a77edd49f5d0e61e4bbc6c8
Bru <annabru.abh@gmail.com>
2019-05-15 12:29:02 +0200

    Created AppContext.js and AppProvider.js

--- src/Components/AppProvider.js
+++ src/Components/AppProvider.js
@@ -2,7 +2,7 @@ import React from "react";
 import AppContext from "./AppContext";
 
 export default class AppProvider extends React.Component {
-  // constructor + state come here
+  // constructor + state come here, but you need constructorrororo
 
   render() {
     return (
