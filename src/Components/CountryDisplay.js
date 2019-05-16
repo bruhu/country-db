6e1785d373e66e3d5a67cdd77c961477e56dce21
Bru <annabru.abh@gmail.com>
2019-05-16 12:05:32 +0200

    Changed title to Country DB - find the tab

--- src/Components/CountryDisplay.js
+++ src/Components/CountryDisplay.js
@@ -5,37 +5,32 @@ export class CountryDisplay extends Component {
   render() {
     return (
       <div className="country-display">
-      
         <AppContext.Consumer>
           {context => {
-
-            
-
-            return(
+            return (
               context.data &&
-                context.data.map((country, index) => (
-                  
-                  <div className="country-display-container" key={index}>
-                    <div className="country-disp-text">
-                      <h3>{country.name}</h3>
-                      <p>
-                        {country.region} - {country.subregion}
-                      </p>
-                      <p>DE: {country.translations.de}</p>
-                      <p>IT: {country.translations.it}</p>
-                      <p>ES: {country.translations.es}</p>
-                      <div>
-                        <img
-                          src={country.flag}
-                          className="country-disp-img"
-                          key={index}
-                          alt="country flag"
-                        />
-                      </div>
+              context.data.map((country, index) => (
+                <div className="country-display-container" key={index}>
+                  <div className="country-disp-text">
+                    <h3>{country.name}</h3>
+                    <p>
+                      {country.region} - {country.subregion}
+                    </p>
+                    <p>DE: {country.translations.de}</p>
+                    <p>IT: {country.translations.it}</p>
+                    <p>ES: {country.translations.es}</p>
+                    <div>
+                      <img
+                        src={country.flag}
+                        className="country-disp-img"
+                        key={index}
+                        alt="country flag"
+                      />
                     </div>
                   </div>
-                ))
-            )              
+                </div>
+              ))
+            );
           }}
         </AppContext.Consumer>
       </div>
